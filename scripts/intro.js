b42e9423ca69aefe0f3000613400a06cb76cf884
Ruann-01 <ruann.campos01@gmail.com>
2020-06-02 21:45:07 -0300

    Incrementando JavaScript

---
+++ scripts/intro.js
@@ -0,0 +1,48 @@
+//document.write("Hello")
+
+// variáveis
+/*
+let myvar //undefined
+const myconst 
+
+const n1 = 1
+const b = true
+
+
+myvar = "Hello"
+
+
+document.write(myvar)
+
+//objeto possuem propriedades e funcionalidades
+const pessoa = {
+  altura : '1,80m',
+  idade: 24, 
+  solteiro: false,
+  correr(){
+    document.write("Correr")
+  }
+}
+
+pessoa.correr()
+
+const colecao = ['Non',34]
+
+function sayMyname(){
+  document.write("My name")
+}
+
+sayMyname()
+
+//Condicionais
+
+if (true){
+  
+}else{
+  
+}
+
+for(i=0;i<10;i++){
+  document.write('<p>${i}</p>')
+}*/
+
