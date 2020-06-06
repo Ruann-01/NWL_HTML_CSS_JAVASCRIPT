349ad16be157b626d26ddef3949e9b0715964a33
Ruann-01 <ruann.campos01@gmail.com>
2020-06-06 16:18:12 -0300

    Continuação do back-end

--- src/server.js
+++ src/server.js
@@ -2,11 +2,18 @@ const express  = require("express")
 
 const server = express()
 
+//configurar pasta pública
+server.use(express.static("public"))
+
 //configurar caminhos da minha aplicação
 //página inicial
 // req:  requisição , res: resposta
 server.get("/",(req,res) => {
-    res.send("Hello World")
+    res.sendFile(__dirname+"/views/index.html")
+})
+
+server.get("/create-point",(req,res)=>{
+    res.sendFile(__dirname+"/views/create-point.html")
 })
 
 
